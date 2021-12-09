877c663e3d246b97d0be92545e271f44354a067d
CEmmanuelP <chrisp3@daum.net>
2021-12-09 16:58:45 +0900

    infinite slider

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Detail from "./Detail";
-import Footer from "./Footer";
-import Header from "./Header";
 import Layout from "./Layout";
 import Login from "./Login";
 import Main from "./Main";
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="detail" element={<Detail />} />
-            <Route path="login" element={<Login />} />
-          </Routes>
-        </Layout>
-      </BrowserRouter>
-    </>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <Layout>
+                    <Routes>
+                        <Route path="/" element={<Main />} />
+                        <Route path="detail" element={<Detail />} />
+                        <Route path="login" element={<Login />} />
+                    </Routes>
+                </Layout>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
--- src/Carousel.js
+++ src/Carousel.js
@@ -1,71 +1,46 @@
 import { useEffect } from "react";
+import "./Carousel.css";
 
 const Carousel = () => {
-  let slideIndex = 1;
+    useEffect(() => {
+        const slider = document.getElementById("slider");
+        const sliderItems = document.getElementById("slides");
+        const prev = document.getElementById("carousel-prev");
+        const next = document.getElementById("carousel-next");
+    });
 
-  useEffect(() => {
-    showSlides(1);
-  });
-  // Next/prev controls
-  const plusSlides = (n) => {
-    showSlides((slideIndex += n));
-  };
+    const slide = (wrapper, items, prev, next) => {
+        let posInitial,
+            index = 0,
+            allowShift = true;
 
-  const showSlides = (n) => {
-    const slides = document.getElementsByClassName("carousel");
-    if (n > slides.length) {
-      slideIndex = 1;
-    }
-    if (n < 1) {
-      slideIndex = slides.length;
-    }
-    for (let i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    slides[slideIndex - 1].style.display = "block";
-  };
+        const slides = items.getElementsByClassName("slide");
+        const slidesLength = slides.length;
+        const slideSize = slides[0].offsetWidth;
+        const firstSlide = slides[0];
+        const lastSlide = slides[slidesLength - 1];
+        const cloneFirst = firstSlide.cloneNode(true);
+        const cloneLast = lastSlide.cloneNode(true);
+    };
 
-  return (
-    <div
-      className="carousel-container"
-      style={{ "margin-left": "5px", "margin-right": "5px" }}
-    >
-      <button className="carousel-prev" onClick={() => plusSlides(-1)}></button>
-      <div className="carousel slide1">
-        <img
-          src="https://img-cf.kurly.com/shop/data/main/1/pc_img_1637311387.jpg"
-          width="100%"
-          height="320"
-          alt="banner"
-        />
-      </div>
-      <div className="carousel slide2">
-        <img
-          src="https://img-cf.kurly.com/banner/main/pc/img/2abed505-2b13-4d4d-878d-98ab6b8bee7a"
-          width="100%"
-          height="320"
-          alt="banner"
-        />
-      </div>
-      <div className="carousel slide3">
-        <img
-          src="https://img-cf.kurly.com/banner/main/pc/img/01bb8bfc-e6b1-4a96-9d67-5afa1df161da"
-          width="100%"
-          height="320"
-          alt="banner"
-        />
-      </div>
-      <div className="carousel slide4">
-        <img
-          src="https://img-cf.kurly.com/banner/main/pc/img/8aea0f43-a2c9-465f-bf74-db2936017e24"
-          width="100%"
-          height="320"
-          alt="banner"
-        />
-      </div>
-      <button className="carousel-next" onClick={() => plusSlides(1)}></button>
-    </div>
-  );
+    return (
+        <div
+            id="slider"
+            className="slider"
+            style={{ "margin-left": "5px", "margin-right": "5px" }}
+        >
+            <div className="wrapper">
+                <button className="carousel-prev"></button>
+                <div className="slides">
+                    <div className="slide"></div>
+                    <div className="slide"></div>
+                    <div className="slide"></div>
+                    <div className="slide"></div>
+                </div>
+                <button className="carousel-next"></button>
+            </div>
+        </div>
+    );
 };
 
 export default Carousel;
