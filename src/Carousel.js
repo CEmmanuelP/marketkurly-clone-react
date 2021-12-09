ec080a02a14a5ba1c3e86a7b10f1b971beea8a21
CEmmanuelP <chrisp3@daum.net>
2021-12-09 17:35:21 +0900

    infinite slide 2

--- src/Carousel.js
+++ src/Carousel.js
@@ -5,8 +5,10 @@ const Carousel = () => {
     useEffect(() => {
         const slider = document.getElementById("slider");
         const sliderItems = document.getElementById("slides");
-        const prev = document.getElementById("carousel-prev");
-        const next = document.getElementById("carousel-next");
+        const prev = document.getElementById("prev");
+        const next = document.getElementById("next");
+
+        slide(slider, sliderItems, prev, next);
     });
 
     const slide = (wrapper, items, prev, next) => {
@@ -21,6 +23,54 @@ const Carousel = () => {
         const lastSlide = slides[slidesLength - 1];
         const cloneFirst = firstSlide.cloneNode(true);
         const cloneLast = lastSlide.cloneNode(true);
+
+        // Clone first and last slide
+        items.appendChild(cloneFirst);
+        items.insertBefore(cloneLast, firstSlide);
+        wrapper.classList.add("loaded");
+
+        // Click events
+        prev.addEventListener("click", () => {
+            shiftSlide(-1);
+        });
+        next.addEventListener("click", () => {
+            shiftSlide(-1);
+        });
+
+        const checkIndex = () => {
+            items.classList.remove("shifting");
+
+            if (index === -1) {
+                items.style.left = -(slidesLength * slideSize) + "px";
+                index = slidesLength - 1;
+            }
+
+            if (index === slidesLength) {
+                items.style.left = -(1 * slideSize) + "px";
+                index = 0;
+            }
+            allowShift = true;
+        };
+
+        // Transition events
+        items.addEventListener("transitionend", checkIndex);
+
+        const shiftSlide = (dir) => {
+            items.classList.add("shifting");
+
+            if (allowShift) {
+                posInitial = items.offsetLeft;
+
+                if (dir === 1) {
+                    items.style.left = posInitial - slideSize + "px";
+                    index++;
+                } else if (dir === -1) {
+                    items.style.left = posInitial + slideSize + "px";
+                    index--;
+                }
+            }
+            allowShift = false;
+        };
     };
 
     return (
@@ -30,14 +80,14 @@ const Carousel = () => {
             style={{ "margin-left": "5px", "margin-right": "5px" }}
         >
             <div className="wrapper">
-                <button className="carousel-prev"></button>
-                <div className="slides">
+                <button id="prev" className="carousel-prev"></button>
+                <div id="slides" className="slides">
                     <div className="slide"></div>
                     <div className="slide"></div>
                     <div className="slide"></div>
                     <div className="slide"></div>
                 </div>
-                <button className="carousel-next"></button>
+                <button id="next" className="carousel-next"></button>
             </div>
         </div>
     );
