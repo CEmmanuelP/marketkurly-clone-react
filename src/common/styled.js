496d27b67dc9cf032dce521f067a3268f7a6d955
CEmmanuelP <chrisp3@daum.net>
2021-12-14 23:39:18 +0900

    responsive layout

--- src/common/styled.js
+++ src/common/styled.js
@@ -1,22 +1,26 @@
 import styled from "styled-components";
 
 export const Main = styled.div`
-    border-bottom: 1px solid #f7f7f7;
+  border-bottom: 1px solid #f7f7f7;
 `;
 
 export const Content = styled.div`
-    min-width: 1050px;
-    padding-bottom: 60px;
+  min-width: 1050px;
+  padding-bottom: 60px;
 `;
 
 export const PageArticle = styled.div`
-    width: 1050px;
-    margin: 0 auto;
+  width: 1050px;
+  margin: 0 auto;
 `;
 
 export const GoodsList = styled.div`
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-gap: 17px;
-    width: 100%;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 17px;
+  width: 100%;
+
+  @media only screen and (max-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
 `;
