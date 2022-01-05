9c135ca3a31746171ceebc45378f0fd0d9b85b0c
CEmmanuelP <chrisp3@daum.net>
2022-01-05 17:57:12 +0900

    cart css 1

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
 import CartItem from "./CartItem";
+import { Content, Main, PageArticle } from "../../common/styled";
 
 const Cart = () => {
     const cart = useSelector((state) => state.cartReducer);
@@ -11,28 +12,44 @@ const Cart = () => {
 
     return (
         <>
-            <div
-                style={{
-                    width: "500px",
-                    height: "500px",
-                    border: "1px solid green",
-                    margin: "0 auto",
-                }}
-            >
-                {/* <p>{cart}</p>
+            <Main>
+                <Content>
+                    <PageArticle>
+                        <div
+                            style={{
+                                paddingTop: "26px",
+                                paddingBottom: "10px",
+                            }}
+                        >
+                            <div style={{ position: "relative" }}>
+                                <h3 className="best_title">장바구니</h3>
+                            </div>
+                        </div>
+                        <div
+                            style={{
+                                border: "1px solid #eee",
+                                width: "500px",
+                                height: "500px",
+                                margin: "0 auto",
+                            }}
+                        >
+                            {/* <p>{cart}</p>
                 <p>{cart.name}</p> */}
-                {cart.map((cartItem) => {
-                    return (
-                        <CartItem
-                            key={cartItem.id}
-                            id={cartItem.id}
-                            name={cartItem.name}
-                            img={cartItem.img}
-                            price={cartItem.price}
-                        />
-                    );
-                })}
-            </div>
+                            {cart.map((cartItem) => {
+                                return (
+                                    <CartItem
+                                        key={cartItem.id}
+                                        id={cartItem.id}
+                                        name={cartItem.name}
+                                        img={cartItem.img}
+                                        price={cartItem.price}
+                                    />
+                                );
+                            })}
+                        </div>
+                    </PageArticle>
+                </Content>
+            </Main>
         </>
     );
 };
